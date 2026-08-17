ac59a762b45dcd0444ed2d249c364ff5f9f1af3d
aaditya755 <aditya15inamke@gmail.com>
2026-08-17 21:23:57 +0530

    2 sum problem and pattern problems but not in a separate folder

---
+++ p10.cpp
@@ -0,0 +1,28 @@
+// problem: 
+/*----
+* * * *
+* * *
+* *
+*     ---
+  */
+
+#include <iostream>
+using namespace std;
+int main()
+
+{   int n=4;
+    int  num=1;
+
+    for(int i=n;i>0;i--){
+            for(int j=i;j>0;j--){
+               
+               cout<<"*"<<" ";
+              
+      
+            }
+
+cout<<endl;
+
+    }
+    return 0;
+}
---
+++ p13.cpp
@@ -0,0 +1,1 @@
+//problem : pyramid pattern apna college
